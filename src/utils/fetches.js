63480b6b4df1019420ff221209545346401038fd
Eys0n <eye3570@naver.com>
2021-06-15 16:20:56 +0900

    Add : fetchesFunc, queryStringParserFunc, storageTokenFunc

--- src/utils/fetches.js
+++ src/utils/fetches.js
@@ -1,18 +1,70 @@
+import { getToken } from './storage';
+
 export const fetchPost = (api, obj) => {
+  const token = getToken();
+
   return fetch(api, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: token,
     },
     body: JSON.stringify(obj),
-  }).then((response) => response.json());
+  });
 };
 
-export const fetchGet = (api, token) => {
+export const fetchDelete = (api) => {
+  const token = getToken();
+
   return fetch(api, {
-    method: 'GET',
+    method: 'DELETE',
+    headers: {
+      Authorization: token,
+    },
+  });
+};
+
+export const fetchGet = (api) => {
+  const token = getToken();
+
+  token
+    ? fetch(api, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token,
+        },
+      })
+    : fetch(api, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+};
+
+export const fetchPut = (api, obj) => {
+  const token = getToken();
+
+  return fetch(`${api}`, {
+    method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: token,
     },
+    body: JSON.stringify(obj),
+  });
+};
+
+export const fetchPatch = (api, obj) => {
+  const token = getToken();
+
+  return fetch(api, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: JSON.stringify(obj),
   }).then((response) => response.json());
 };
