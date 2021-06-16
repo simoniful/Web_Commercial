6fd10f6a3c951b9a24faa6e69f4c93e375db4f01
sanghunlee <simoniful0gmail.com>
2021-06-16 15:41:54 +0900

    Add: CSS 수정

--- src/Pages/Main/Order/index.js
+++ src/Pages/Main/Order/index.js
@@ -114,6 +114,10 @@ export default class Order extends Component {
                   출고 후 평균 1~2일 이내 수령
                   <br />
                   영업일 기준이며, 경우에 따라 추가소요될 수 있습니다.
+                  <br />
+                  택배사 파업으로 일부 지역의 배송이 일시적으로 중단됩니다.
+                  <br />
+                  (중단 지역은 “공지사항“에서 확인이 가능합니다.)
                 </div>
               </div>
             </div>
@@ -123,17 +127,42 @@ export default class Order extends Component {
             <div className="paymentWrap">
               <OrderPrice totalPrice={totalPrice} />
               <div className="payMethod">
-                <h4>결제수단 선택</h4>
+                <h4 className="subtitle">결제수단 선택</h4>
                 <div className="selectBox">
-                  <input type="radio" />
-                  <input type="radio" />
+                  <label for="kakaopay" className="">
+                    카카오페이
+                    <input
+                      id="kakaopay"
+                      type="radio"
+                      value="kakaopay"
+                      checked=""
+                    />
+                  </label>
+                  <label for="creditcard" className="">
+                    신용카드
+                    <input
+                      id="creditcard"
+                      type="radio"
+                      value="creditcard"
+                      checked=""
+                    />
+                  </label>
+                </div>
+                <div className="agreeBox">
+                  <label className="agreeToNotice">
+                    <input type="checkbox" />
+                    상품 주문 및 배송정보 수집에 동의합니다<span>[필수]</span>
+                  </label>
+                  <label className="agreeToNotice">
+                    <input type="checkbox" />
+                    주문 상품의 명시내용과 사용조건을 확인하였으며, 취소환불
+                    규정에 동의합니다<span>[필수]</span>
+                  </label>
                 </div>
                 <div>
-                  <input type="checkbox"></input>
-                  <input type="checkbox"></input>
-                </div>
-                <div>
-                  <button type="submit">결제하기</button>
+                  <button type="submit" className="submitBtn">
+                    결제하기
+                  </button>
                 </div>
               </div>
             </div>
